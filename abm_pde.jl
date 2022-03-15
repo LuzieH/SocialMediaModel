db0bec05c2a6804216d763b4fe65f04d1e722e7b
LuzieH <luziehelfmann@gmail.com>
2022-03-15 12:20:52 +0100

    numerics not working, started adding bc for divergence term

--- abm_pde.jl
+++ abm_pde.jl
@@ -42,28 +42,23 @@ const m_1 = 1/2 #proportion of agents of type 1,2
 const m_2 = 1/2 
 
 # interaction with other agents
-function agent_interaction(rho_s, rho_other)
-    rho = rho_s + rho_other
+function agent_force(rho)
    # @show size(rho), N
     #pointwise_int = dV*sum(vec(rho).*K_matrix, dims=1)
 
     pointwise_int = zeros(N, 2)
-    for d in 1:2, j in 1:N, i in 1:N
+    for d in 1:2, i in 1:N, j in 1:N
         pointwise_int[i,d] += rho[j] * K_matrix[j, i, d]
     end
     pointwise_int .*= dV
 
-    force = rho_s .* reshape(pointwise_int, N_y, N_x, 2)
+    force = reshape(pointwise_int, N_y, N_x, 2)
     return force
 end
 
 # interaction with media
-function media_interation(rho_s, z)
-   #@show size(grid_points), size(z)
+function media_force(z)
     pointwise_int =   z' .- grid_points
-   #@show size(pointwise_int)
-    force = reshape(rho_s,N) .* pointwise_int
-   # @show size(force), N_y, N_y, N
     force = reshape(force,N_y,N_x, 2)
     return force
 end
@@ -79,11 +74,22 @@ My[end,end-1] = 2.0
 Mx .= (1/dx^2)*Mx
 My .= (1/dy^2)*My
 
-#centered first difference
+#centered first difference for force
 const Cx = Array(Tridiagonal([1.0 for i in 1:N_x-1],[0. for i in 1:N_x],[-1.0 for i in 1:N_x-1]))
 const Cy = -copy(Cx)
 Cx .= 1/(2*dx)*Cx
 Cy .= 1/(2*dy)*Cy
+Cy[1,1:2] = 1/(dy)* [-1,1]
+Cy[end,end-1:end] = 1/(dy)* [-1,1]
+Cy[1:2,1] = 1/(dx)* [-1,1]
+Cy[end-1:end,end] = 1/(dx)* [-1,1]
+
+#centered first difference for density
+const Cx_rho = Array(Tridiagonal([1.0 for i in 1:N_x-1],[0. for i in 1:N_x],[-1.0 for i in 1:N_x-1]))
+const Cy_rho = -copy(Cx)
+Cx_rho .= 1/(2*dx)*Cx
+Cy_rho .= 1/(2*dy)*Cy
+Cx_rho[2,1]=0. ; Cx_rho[end-1,end]=0.; Cy_rho[1,2]=0.; Cy_rho[end,end-1]=0.
 
 # Define the discretized PDE as an ODE function
 function f(duz,uz,p,t)
@@ -105,10 +111,12 @@ function f(duz,uz,p,t)
 
   #@show size(z_1)
 
-  force_1 = c*media_interation(rho_1, z_1) + a*agent_interaction(rho_1, rho_2)  
-  force_2 = c*media_interation(rho_2, z_2) + a*agent_interaction(rho_2, rho_1)  
-  drho_1 .= D*(My*rho_1 + rho_1*Mx) - (Cy*force_1[:,:,2] + force_1[:,:,1]*Cx)
-  drho_2 .= D*(My*rho_2 + rho_2*Mx) - (Cy*force_2[:,:,2] + force_2[:,:,1]*Cx)
+  force_1 = c*media_force(z_1) + a*agent_force(rho_1+ rho_2)
+  force_2 = c*media_force(z_2) + a*agent_force(rho_1+ rho_2)
+  div_1 = rho_1 .* (Cy*force_1[:,:,2] + force_1[:,:,1]*Cx) 
+  div_2 = rho_2 .* (Cy*force_2[:,:,2] + force_2[:,:,1]*Cx)
+  drho_1 .= D*(My*rho_1 + rho_1*Mx) - div_1
+  drho_2 .= D*(My*rho_2 + rho_2*Mx) - div_2
   mean_rho_1 = 1/m_1 * reshape(rho_1,1,N)*grid_points
   mean_rho_2 = 1/m_2 * reshape(rho_2,1,N)*grid_points
   dz_1 .= 1/(Gamma_0*m_1) * (mean_rho_1' - z_1)
@@ -119,7 +127,7 @@ uz0 = ArrayPartition(u0,z0)
 
 
 # Solve the ODE
-prob = ODEProblem(f,uz0,(0.0,1))
+prob = ODEProblem(f,uz0,(0.0,0.01))
 @time sol = solve(prob,progress=true,save_everystep=true,save_start=false)
 
 using Plots;
