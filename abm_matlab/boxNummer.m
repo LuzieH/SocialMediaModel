d650b07de4c943e17ca9946959e328be1dd9eece
Luzie Helfmann <luziehelfmann@gmail.com>
2023-01-27 10:40:38 +0100

    Delete abm_matlab directory

--- abm_matlab/boxNummer.m
+++
@@ -1,6 +0,0 @@
-function boxnr = boxNummer(x,n,h)
-
-for i=1:n
-    boxnr(1,i) = fix((x(1,i)+2)/h)+1;
-    boxnr(2,i) = fix((x(2,i)+2)/h)+1;
-end
--- abm_matlab/boxNummerI.m
+++
@@ -1,6 +0,0 @@
-function boxnr = boxNummerI(influencer,h)
-
-for i=1:4
-    boxnr(1,i) = fix((influencer(1,i)+2)/h)+1;
-    boxnr(2,i) = fix((influencer(2,i)+2)/h)+1;
-end
