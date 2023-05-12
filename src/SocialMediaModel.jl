8a3f9099c4a95fefc5975075cbfb3139feb88037
Luzie Helfmann <20030747+LuzieH@users.noreply.github.com>
2023-05-12 16:59:54 +0200

    updated module

--- src/SocialMediaModel.jl
+++ src/SocialMediaModel.jl
@@ -7,10 +7,11 @@ module SocialMediaModel
     include("controlstrategies.jl")
     include("strategyexperiments.jl")
     include("tests.jl")
+    include("paperfigures.jl")
 
     export  ABMsolve,  ABMsolveplot, PDEsolve, PDEsolveplot, PDEplotsnapshots, ABMplotsnapshots, PDEgifsingle,
      ABMplotfollowernumbers, ABMgifsingle, plotensemblesnapshots, PDEsolveensemble, ABMsolveensemble, runensembles,
      parameters, parametersstronginf, ABMconstruct, PDEconstruct, PDEconstructcoarse, PDEconstructmeso, parameterscontrol,
      PDEsolvefixedtargets, PDEsolveopt, GLmed, GLinf, infrightcorner, infcounteraction, medcounteraction,
-     testabm, testpde, testensemble, testcontrol, testoptimization, runtests
+     testabm, testpde, testensemble, testcontrol, testoptimization, runtests, paperfigures
 end 
